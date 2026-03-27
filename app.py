542c0476351f61c01ad3cbdfbdcb8b12c15a7662
RahanaPA <rahanapgm@gmail.com>
2026-03-27 23:23:08 +0400

    Added all files

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask  import Flask,render_template,jsonify,request
+from src.helper import download_huggingface_embeddings
+from langchain_pinecone import PineconeVectorStore
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from langchain.llms import HuggingFacePipeline
+
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+from dotenv import load_dotenv
+
+from src.prompt import *
+import os
+
+app =Flask(__name__)
+
+load_dotenv()
+
+# OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+HF_API_KEY= os.getenv("HF_API_KEY")
+PINECONE_API_KEY= os.getenv("PINECONE_API_KEY")
+
+# os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
+os.environ["HF_API_KEY"]= HF_API_KEY
+os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
+
+embeddings = download_huggingface_embeddings()
+
+index_name = "medical-chatbot"
+#Embed each chunk and upsert the embeddings into your Pinecone index
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+
+retriever = docsearch.as_retriever(search_type="similarity",search_kwargs={"k":3})
+
+model_name = "google/flan-t5-large"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+pipe = pipeline(
+    task="text2text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    
+    temperature=0.2
+)
+
+chatModel = HuggingFacePipeline(pipeline=pipe)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",system_prompt),
+        ("human","{input}")
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel,prompt)
+rag_chain=create_retrieval_chain(retriever,question_answer_chain)
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get",methods=["GET","POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input":msg})
+    print("Response :",response["answer"])
+    return str(response["answer"])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug =True)
---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "Ypu are a Medical assistant for question-answering tasks."
+    "Use the following pieces of retrieved context to answer the question."
+    "If you dont know the answer, say that you don't know."
+    "Use three sentences maximum and keep the answer concise."
+    "\n\n"
+    "{context}"
+)
